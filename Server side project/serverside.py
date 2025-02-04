96789d185bbb173ac6d59946197fe7cc72a6ff91
omari-elkanah <elkanahomari100@gmail.com>
2025-02-04 14:31:06 +0300

    barebones

--- Server side project/serverside.py
+++ Server side project/serverside.py
@@ -1,3 +1,25 @@
+#barebones
+def listen():
+    #listen for new connected devices on dhcp server
+    #if connection is made, collect ip and mac address
+    #find method to store list of mac addresses
+    return "listen"
+
 def Inquire():
-    
+    #get listen variables
+    #get target name
+    #get mac address
+    #get os version
     return "Inquire"
+
+if Inquire.version != #win 11 version number
+    #load storage area
+    #update in background; give bool(1) for mac address; store
+    #wait for update from client and check if complete
+    if update == True:#complete
+        #give bool (1)
+    else:
+        #give bool (0)
+else:
+    #give bool (1) because it is win 11
+    
